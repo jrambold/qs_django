5b9be217f3d8e5f79e5c44c8840670cc400be64a
jrambold <jrambold@gmail.com>
2018-07-05 10:52:14 -0600

    setup matching views to endpoints

--- diary/views.py
+++ diary/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, JsonResponse
+from teams.models import Food, Meal
+from django.core import serializers
+import requests
+import json
+import os
 
 # Create your views here.
+
+def food_index(request):
+    return JsonResponse({'Hello': 'World'})
+
+def food_show(request):
+    return JsonResponse({'Hello': 'World'})
+
+def meal_index(request):
+    return JsonResponse({'Hello': 'World'})
+
+def meal_show(request):
+    return JsonResponse({'Hello': 'World'})
+
+def mf_show(request):
+    return JsonResponse({'Hello': 'World'})
