e8843030f4d7b4328ad35fcd647b5d36b56cbf68
jrambold <jrambold@gmail.com>
2018-07-05 12:47:02 -0600

    Create Food Endpoint added

--- diary/views.py
+++ diary/views.py
@@ -2,15 +2,25 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, JsonResponse
 from diary.models import Food, Meal
 from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
 import requests
 import json
 import os
 
 # Create your views here.
 
+@csrf_exempt
 def food_index(request):
-	foods = serializers.serialize("json", Food.objects.all())
-	return JsonResponse(json.loads(foods), safe=False)
+    if request.method == 'GET':
+    	foods = serializers.serialize("json", Food.objects.all())
+    	return JsonResponse(json.loads(foods), safe=False)
+    elif request.method == 'POST':
+        food_data = json.loads(request.body)['food']
+        food = Food(name = food_data['name'], calories = food_data['calories'])
+        food.save()
+
+        food = serializers.serialize("json", Food.objects.filter(id=food.id))
+        return JsonResponse(json.loads(food), safe=False)
 
 def food_show(request, food_id):
 	food = serializers.serialize("json", Food.objects.filter(id=food_id))
