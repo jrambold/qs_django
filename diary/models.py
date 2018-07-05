2012f784fb55bc47f7ecdf9da5182857ac98d359
jrambold <jrambold@gmail.com>
2018-07-05 11:33:25 -0600

    string returns for models

--- diary/models.py
+++ diary/models.py
@@ -6,6 +6,12 @@ class Food(models.Model):
     name = models.CharField(max_length=200)
     calories = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+
 class Meal(models.Model):
     name = models.CharField(max_length=200)
     foods = models.ManyToManyField(Food)
+
+    def __str__(self):
+        return self.name
