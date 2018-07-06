2ac946f7e93c911c087594954da135f26b9cce3b
jrambold <jrambold@gmail.com>
2018-07-05 18:44:41 -0600

    added cors and fixed urls to match fe

--- diary/urls.py
+++ diary/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('foods', views.food_index, name='food_index'),
-	path('foods/<int:food_id>', views.food_show, name='food_show'),
-	path('meals', views.meal_index, name='meal_index'),
-	path('meals/<int:meal_id>/foods', views.meal_show, name='meal_show'),
-	path('meals/<int:meal_id>/foods/<int:food_id>', views.mf_show, name='mf_show'),
+	path('foods/', views.food_index, name='food_index'),
+	path('foods/<int:food_id>/', views.food_show, name='food_show'),
+	path('meals/', views.meal_index, name='meal_index'),
+	path('meals/<int:meal_id>/foods/', views.meal_show, name='meal_show'),
+	path('meals/<int:meal_id>/foods/<int:food_id>/', views.mf_show, name='mf_show'),
 ]
--- qs_django/settings.py
+++ qs_django/settings.py
@@ -41,9 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'diary',
     'dj_database_url',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +56,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'qs_django.urls'
 
